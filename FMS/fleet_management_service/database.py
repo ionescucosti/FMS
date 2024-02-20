ff60274a06ba31edbf72ee24c674379df807853d
ionescucosti <122608006+ionescucosti@users.noreply.github.com>
2024-02-20 10:06:57 +0200

    Delete FMS directory

--- FMS/fleet_management_service/database.py
+++
@@ -1,9 +0,0 @@
-from databases import Database
-from sqlalchemy import create_engine, MetaData
-
-DATABASE_URL = "sqlite:///./fleet.db"
-database = Database(DATABASE_URL)
-metadata = MetaData()
-
-engine = create_engine(DATABASE_URL)
-metadata.create_all(engine)
